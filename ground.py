e06a980ea9459a0ce60d4bb4055e958bb583ef76
Rolando Garcia <rogarcia@berkeley.edu>
2017-08-18 16:14:39 -0700

    Finished ground client API for tutorial

--- ground.py
+++ ground.py
@@ -9,6 +9,7 @@ class GroundClient:
 		self.port = str(port)
 		self.url = "http://" + self.host + ":" + self.port
 
+	### EDGES ###
 	def createEdge(self, sourceKey, fromNodeId, toNodeId, name="null"):
 		d = {
 			"sourceKey": sourceKey,
@@ -19,6 +20,21 @@ class GroundClient:
 		requests.post(self.url + "/edges", headers=self.headers, 
 			data=json.dumps(d))
 
+	def createEdgeVersion(self, edgeId, toNodeVersionStartId):
+		d = {
+			"edgeId": edgeId,
+			"toNodeVersionStartId": toNodeVersionStartId
+		}
+		requests.post(self.url + "/versions/edges", headers=self.headers,
+			data=json.dumps(d))
+
+	def getEdge(self, sourceKey):
+		return requests.get(self.url + "/edges/" + str(sourceKey)).json()
+
+	def getEdgeVersion(self, edgeId):
+		return requests.get(self.url + "/versions/edges" + str(edgeId)).json()
+
+	### NODES ###
 	def createNode(self, sourceKey, name="null"):
 		d = {
 			"sourceKey": sourceKey,
@@ -36,4 +52,31 @@ class GroundClient:
 
 	def getNode(self, sourceKey):
 		return requests.get(self.url + "/nodes/" + str(sourceKey)).json()
+
+	def getNodeVersion(self, nodeId):
+		return requests.get(self.url + "/versions/nodes/" + str(nodeId)).json()
+
+	### GRAPHS ###
+	def createGraph(self, sourceKey, name="null"):
+		d = {
+			"sourceKey": sourceKey,
+			"name": name
+		}
+		requests.post(self.url + "/graphs", headers=self.headers, 
+			data=json.dumps(d))
+
+	def createGraphVersion(self, graphId, edgeVersionIds):
+		d = {
+			"graphId": graphId,
+			"edgeVersionIds": edgeVersionIds
+		}
+		requests.post(self.url + "/versions/graphs", headers=self.headers,
+			data=json.dumps(d))
+
+	def getGraph(self, sourceKey):
+		return requests.get(self.url + "/graphs/" + str(sourceKey)).json()
+
+	def getGraphVersion(self, graphId):
+		return requests.get(self.url + "/versions/graphs/" + str(graphId)).json()
+
 
