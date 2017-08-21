fd61d9b63d1811014cc458620a6ad72da10bbc6e
Rolando Garcia <rogarcia@berkeley.edu>
2017-08-20 20:51:05 -0700

    Intermid

--- driver.py
+++ driver.py
@@ -1,22 +1,59 @@
 from ground import GroundClient
 
+# engineerTags: {
+#       "name": {
+#           "key": "name", 
+#           "value": "an-engineer", 
+#           "type": "string"
+#       },
+#       "age": {
+#           "key": "age", 
+#           "value": 25, 
+#           "type": "integer"
+#       }, 
+#       "department": {
+#           "key": "department", 
+#           "value": "engineering", 
+#           "type": "string"
+#       }
+#   }
+
+# original_schema = {
+# 	"id" : "integer id of the tweet",
+# 	"tweet" : "text of the tweet",
+# 	"place" : "city state from which the tweet originated",
+# 	"city" : "city from which the tweet originated",
+# 	"country" : "country from which the tweet originated",
+# 	"code": "two character country code",
+# 	"training" : "boolean whether record is used for training"
+# }
+
+# alternative_schema = {
+# 	"id" : "integer id of the tweet",
+# 	"tweet" : "text of the tweet",
+# 	"code" : "country from which the tweet originated",
+# 	"city" : "city from which the tweet originated",
+# 	"country" : "two character country code",
+# 	"training" : "boolean whether record is used for training"
+# }
+
 original_schema = {
-	"id" : "integer id of the tweet",
-	"tweet" : "text of the tweet",
-	"place" : "city state from which the tweet originated",
-	"city" : "city from which the tweet originated",
-	"country" : "country from which the tweet originated",
-	"code": "two character country code",
-	"training" : "boolean whether record is used for training"
+	"id",
+	"tweet",
+	"place",
+	"city",
+	"country",
+	"code",
+	"training"
 }
 
 alternative_schema = {
-	"id" : "integer id of the tweet",
-	"tweet" : "text of the tweet",
-	"code" : "country from which the tweet originated",
-	"city" : "city from which the tweet originated",
-	"country" : "two character country code",
-	"training" : "boolean whether record is used for training"
+	"id",
+	"tweet",
+	"code",
+	"city",
+	"country",
+	"training" 
 }
 
 client = GroundClient()
